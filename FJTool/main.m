8a4f0936ed72a661ecac4b0681045b560611cb45
tribbleofjim <lyifee@qq.com>
2020-12-23 17:53:42 +0800

    Initial Commit

---
+++ FJTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FJTool
+//
+//  Created by yifee L on 2020/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
